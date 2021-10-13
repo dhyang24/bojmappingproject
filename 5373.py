3e5411c9b29dec0c917a6c44e499b39ade6397bd
Dongho Yang <helium050530@naver.com>
2021-10-13 19:39:30 +0900

    d

--- 5373.py
+++
@@ -1,9 +0,0 @@
-while True:
-    a, b = map(int, input().split())
-    if(a,b) == (0,0):break
-    c = [0 for i in range(a)]
-    for i in range(b):
-        d = list(map(int, input().split()))
-        for p in range(len(d)):
-            c[p] += d[p]
-    print("yes" if b in c else 'no')
