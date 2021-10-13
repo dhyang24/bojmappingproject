ea82f0339ee82a10a8bca0996fc59f6364a06816
Dongho Yang <helium050530@naver.com>
2021-10-13 20:04:34 +0900

    a

---
+++ codes/5789.py
@@ -0,0 +1,3 @@
+for _ in range(int(input())):
+    a = input()
+    print("Do-it" if a[len(a)//2-1] == a[len(a)//2] else "Do-it-Not")
---
+++ codes/5919.py
@@ -0,0 +1,8 @@
+a = []
+for i in range(int(input())):
+    a.append(int(input()))
+avg = sum(a)//len(a)
+thing = 0
+for i in a:
+    thing+=abs(avg-i)
+print(thing//2)
