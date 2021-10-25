b600b7228948f8efa382a1e9463ccb7f37eee0d1
Dongho Yang <helium050530@naver.com>
2021-10-25 21:02:49 +0900

    \

--- codes/9896.cpp
+++ codes/9896.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main(void):
+int main(void){
+    int a;
+    cin >> a;
+    string b;
+    cin >> b;
+    string ans;
+    ans = b[0];
+    for (int i=1; i<a; i++){
+        ans.push_back((b[i]-'0')^(b[i-1]-'0')+'0');
+    }
+    cout << ans;
+}
