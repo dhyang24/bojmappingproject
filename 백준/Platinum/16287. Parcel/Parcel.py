0ab83427d87df411a74b1a630d64b94fe306663d
dhyang24 <57130121+dhyang24@users.noreply.github.com>
2023-10-26 15:01:42 +0900

    [Platinum V] Title: Parcel, Time: 840 ms, Memory: 178192 KB -BaekjoonHub

--- 백준/Platinum/16287. Parcel/Parcel.py
+++ 백준/Platinum/16287. Parcel/Parcel.py
@@ -1,9 +1,3 @@
-a,b = map(int,input().split())
-c = list(map(int,input().split()))
-d = [0,0,0,0]
-for i in c:
-    d[3] = d[3]|(d[2]<<i)
-    d[2] = d[2]|(d[1]<<i)
-    d[1] = d[1]|(d[0]<<i)
-    d[0] = d[0]|(1<<i)
-print("YES" if d[3]&(1<<a) else "NO")
+a,b=map(int,input().split());c=list(map(int,input().split()));d=[0,0,0,0]
+for i in c:d=[d[0]|(1<<i),d[1]|(d[0]<<i),d[2]|(d[1]<<i),d[3]|(d[2]<<i)]
+print("YES"if d[3]&(1<<a) else"NO")
