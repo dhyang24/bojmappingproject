15ba8bc76df5333faf5eb67f1294526e702fba58
dhyang24 <57130121+dhyang24@users.noreply.github.com>
2023-10-27 15:47:29 +0900

    [Bronze V] Title: Lucky 7, Time: 140 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30224. Lucky 7/Lucky 7.py
@@ -0,0 +1,9 @@
+a = input()
+if "7" not in a and int(a)%7 != 0:
+    print(0)
+elif "7" not in a and int(a)%7 == 0:
+    print(1)
+elif "7" in a and int(a)%7 != 0:
+    print(2)
+elif "7" in a and int(a)%7 == 0:
+    print(3)
